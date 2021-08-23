75787adcc41e69703368302b78882a3ce131114d
Upasana Dhameliya <upasana.dhameliya@gmail.com>
2021-08-23 09:46:30 +0530

    Individual link redirection successful

--- web1/first_try.py
+++ web1/first_try.py
@@ -6,30 +6,45 @@ import time
 driver = webdriver.Chrome("D:\\upa\\downloads\\chromedriver_win32\\chromedriver.exe")
 
 driver.get("https://www.cyprusbar.org/CypriotAdvocateMembersPage.aspx")
-
-
-# for ind,_ in enumerate(driver.find_elements_by_class_name('dxb-hb'),0):
-#     print(ind)
-#     breakpoint()
 try:
     eng = driver.find_element_by_name('ctl00$ctl17')
-    # breakpoint()
     ActionChains(driver).move_to_element(eng).click(eng).perform()
 except:
     pass
 
 
-element = driver.find_element_by_class_name('dxb-hb')
+for ind,_ in enumerate(driver.find_elements_by_class_name('dxb-hb'),0):
+    time.sleep(2)
+
+    element = driver.find_elements_by_class_name('dxb-hb')[ind]
+    ActionChains(driver).move_to_element(element).click(element).perform()
+    time.sleep(2)
+    driver.back()
+
+
+# try:
+#     eng = driver.find_element_by_name('ctl00$ctl17')
+#     ActionChains(driver).move_to_element(eng).click(eng).perform()
+# except:
+#     pass
+
+
+# element = driver.find_element_by_class_name('dxb-hb')
+# ActionChains(driver).move_to_element(element).click(element).perform()
+# time.sleep(2)
+# driver.back()
+
+
 # driver.implicitly_wait(10)
-ActionChains(driver).move_to_element(element).click(element).perform()
-    # # driver.get("https://www.cyprusbar.org/CypriotAdvocateMembersPage.aspx")
+
+# # driver.get("https://www.cyprusbar.org/CypriotAdvocateMembersPage.aspx")
 # driver.implicitly_wait(10)
-time.sleep(2)
+
 
 # driver.execute_script("window.history.go(-1)")
 # driver.get("https://www.cyprusbar.org/CypriotAdvocateMembersPage.aspx")
 # driver.navigate().to("https://www.cyprusbar.org/CypriotAdvocateMembersPage.aspx")
-driver.back()
+
 # driver.implicitly_wait(10)
 
 # breakpoint()
