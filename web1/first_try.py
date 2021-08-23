4a0f4fa4cce8875dc82cd566eac6c1286a79af82
Upasana Dhameliya <upasana.dhameliya@gmail.com>
2021-08-23 09:34:39 +0530

    Initial Commit

---
+++ web1/first_try.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+
+driver = webdriver.Chrome("D:\\upa\\downloads\\chromedriver_win32\\chromedriver.exe")
+
+driver.get("https://www.cyprusbar.org/CypriotAdvocateMembersPage.aspx")
+
+
+# for ind,_ in enumerate(driver.find_elements_by_class_name('dxb-hb'),0):
+#     print(ind)
+#     breakpoint()
+try:
+    eng = driver.find_element_by_name('ctl00$ctl17')
+    # breakpoint()
+    ActionChains(driver).move_to_element(eng).click(eng).perform()
+except:
+    pass
+
+
+element = driver.find_element_by_class_name('dxb-hb')
+# driver.implicitly_wait(10)
+ActionChains(driver).move_to_element(element).click(element).perform()
+    # # driver.get("https://www.cyprusbar.org/CypriotAdvocateMembersPage.aspx")
+# driver.implicitly_wait(10)
+
+# driver.execute_script("window.history.go(-1)")
+# driver.get("https://www.cyprusbar.org/CypriotAdvocateMembersPage.aspx")
+# driver.navigate().to("https://www.cyprusbar.org/CypriotAdvocateMembersPage.aspx")
+# driver.back()
+# driver.implicitly_wait(10)
+
+# breakpoint()
