109a8cdc545d0b3b0cea06b9c2930587783d30d9
Upasana Dhameliya <upasana.dhameliya@gmail.com>
2021-08-26 10:22:48 +0530

    Added iteration logic
    Each row of the table will be iterated for the later
    retrieval of data

--- davac/davac_scrape.py
+++ davac/davac_scrape.py
@@ -14,6 +14,25 @@ driver.find_element_by_link_text("English").click()
 time.sleep(1)
 element = driver.find_element(By.XPATH, "//button[@type='submit' and contains(., 'Search')]")
 ActionChains(driver).move_to_element(element).click(element).perform()
-time.sleep(10)
+try:
+    element = WebDriverWait(driver, 60).until(
+        EC.presence_of_element_located((By.XPATH, "//table[contains(@class, 'searchGridTab')]"))
+    )
+
+except:
+    print("Could not find table")
+    exit(0)
 driver.get("http://vyhledavac.cak.cz/Home/SearchResult?page=1&pageSize=40000")
-# http://vyhledavac.cak.cz/Home/SearchResult?page=1&pageSize=40000
+
+table = driver.find_element_by_class_name("searchGridTab")
+print(table)
+
+table_rows = table.find_elements_by_tag_name('tr')
+print("table_rows found!")
+
+def store_data(*args):
+    print(args)
+
+for r_ind, row in enumerate(table_rows,1):
+    store_data(*[cell.text for c_ind, cell in enumerate(row.find_elements_by_tag_name('td'),1) if c_ind !=6])
+    if r_ind == 5: break
